ac12f497fb780497810831d89830177b47a7fc42
davidschraedel <65734436+davidschraedel@users.noreply.github.com>
2021-02-16 23:52:57 -0700

    Add project files.

---
+++ Models/Book.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineBookstore.Models
+{
+    public class Book
+    {
+        [Key]
+        public int BookId {get;set;}
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string AuthorGivenName { get; set; }
+        [Required]
+        public string AuthorSurname { get; set; }
+        [Required]
+        public string Publisher { get; set; }
+        [Required]
+        [RegularExpression(@"^[\d]{3}-[\d]{10}$", ErrorMessage = "ISBN must be in correct format: ###-##########")]
+        public string ISBN { get; set; }
+        [Required]
+        public string Classification { get; set; }
+        [Required]
+        public string Category { get; set; }
+        [Required]
+        [RegularExpression(@"^\$?([0-9]{1,3},([0-9]{3},)*[0-9]{3}|[0-9]+)(.[0-9][0-9])?$", ErrorMessage = "Price must be in a correct currency format: (e.g. $25.95)")]
+        
+        public decimal Price { get; set; }
+    }
+}
---
+++ Models/SeedData.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineBookstore.Models
+{
+    public class SeedData
+    {
+        public static void EnsurePopulated(IApplicationBuilder application)
+        {
+            BookDbContext context = application.ApplicationServices.CreateScope().ServiceProvider.GetRequiredService<BookDbContext>();
+
+            if (context.Database.GetPendingMigrations().Any())
+            {
+                context.Database.Migrate();
+            }
+            if (!context.Books.Any())
+            {
+                context.Books.AddRange(
+                    new Book
+                    {
+                        Title = "Les Miserables",
+                        AuthorGivenName = "Victor",
+                        AuthorSurname = "Hugo",
+                        Publisher = "Signet",
+                        ISBN = "978-0451419439",
+                        Classification = "Fiction",
+                        Category = "Classic",
+                        Price = 9.95m
+                    },
+                    new Book
+                    {
+                        Title = "Team of Rivals",
+                        AuthorGivenName = "Doris Kearns",
+                        AuthorSurname = "Goodwin",
+                        Publisher = "Simon & Schuster",
+                        ISBN = "978-0743270755",
+                        Classification = "Non-Fiction",
+                        Category = "Biography",
+                        Price = 14.58m
+                    },
+                    new Book
+                    {
+                        Title = "The Snowball",
+                        AuthorGivenName = "Alice",
+                        AuthorSurname = "Schroeder",
+                        Publisher = "Bantam",
+                        ISBN = "978-0553384611",
+                        Classification = "Non-Fiction",
+                        Category = "Biography",
+                        Price = 21.54m
+                    },
+                    new Book
+                    {
+                        Title = "American Ulysses",
+                        AuthorGivenName = "Ronald C.",
+                        AuthorSurname = "White",
+                        Publisher = "Random House",
+                        ISBN = "978-0812981254",
+                        Classification = "Non-Fiction",
+                        Category = "Biography",
+                        Price = 11.61m
+                    },
+                    new Book
+                    {
+                        Title = "Unbroken",
+                        AuthorGivenName = "Laura",
+                        AuthorSurname = "Hillenbrand",
+                        Publisher = "Random House",
+                        ISBN = "978-0812974492",
+                        Classification = "Non-Fiction",
+                        Category = "Historical",
+                        Price = 13.33m
+                    },
+                    new Book
+                    {
+                        Title = "The Great Train Robbery",
+                        AuthorGivenName = "Michael",
+                        AuthorSurname = "Chrichton",
+                        Publisher = "Vintage",
+                        ISBN = "978-0804171281",
+                        Classification = "Fiction",
+                        Category = "Historical Fiction",
+                        Price = 15.95m
+                    },
+                    new Book
+                    {
+                        Title = "Deep Work",
+                        AuthorGivenName = "Cal",
+                        AuthorSurname = "Newport",
+                        Publisher = "Grand Central Publishing",
+                        ISBN = "978-1455586691",
+                        Classification = "Non-Fiction",
+                        Category = "Self-Help",
+                        Price = 14.99m
+                    },
+                    new Book
+                    {
+                        Title = "It\'s Your Ship",
+                        AuthorGivenName = "Michael",
+                        AuthorSurname = "Abrashoff",
+                        Publisher = "Grand Central Publishing",
+                        ISBN = "978-1455523023",
+                        Classification = "Non-Fiction",
+                        Category = "Self-Help",
+                        Price = 21.66m
+                    },
+                    new Book
+                    {
+                        Title = "The Virgin Way",
+                        AuthorGivenName = "Richard",
+                        AuthorSurname = "Branson",
+                        Publisher = "Portfolio",
+                        ISBN = "978-1591847984",
+                        Classification = "Non-Fiction",
+                        Category = "Business",
+                        Price = 29.16m
+                    },
+                    new Book
+                    {
+                        Title = "Sycamore Row",
+                        AuthorGivenName = "John",
+                        AuthorSurname = "Grisham",
+                        Publisher = "Bantam",
+                        ISBN = "978-0553393613",
+                        Classification = "Fiction",
+                        Category = "Thrillers",
+                        Price = 15.03m
+                    }
+                );
+
+                context.SaveChanges();
+            }    
+        }
+    }
+}
